b7ed8ce1b2c9a62c25932d73750ad5eb37ab4c36
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-08-22 17:16:18 -0300

    finalizaition

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-"Vamos criar juntos"
+import express from "express"
+import {config} from "dotenv"
+
+config()
+
+const serverPort = process.env.PORT || 3000
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    return res.status(200).send({ message:"hello word!"})
+})
+
+app.listen(serverPort, () => {
+    console.log(`🙅‍♂️🙇‍♀️👌🤰 Server starded on http://localhost:${serverPort}`)
+})
