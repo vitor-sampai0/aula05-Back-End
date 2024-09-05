d658e902fa523980ca35e8a96f7b1c7988cad063
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-09-05 17:20:36 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,13 +20,13 @@ let emocoes = [
     },
 
 ]
-
+// rota para buscar todas as emoções 
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes)
 });
-
+// Rota para criar uma nova Emoção
 emocoesRoutes.post("/", (req, res) => {
-    const {nome, cor} = req.body
+    const { nome, cor } = req.body
     const newemocao = {
         id: emocoes.length + 1,
         nome: nome,
@@ -34,6 +34,25 @@ emocoesRoutes.post("/", (req, res) => {
     }
     emocoes.push(newemocao)
     return res.status(201).send(emocoes)
+});
+// Rota para buscar uma emoção pelo id
+emocoesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    console.log(id);
+    const emocao = emocoes.find((emotion) => emotion .id == id)
+
+    if(!emocao) {
+        return res.status(404).send({
+            message : "emoção não encontrada!",
+        })
+    }
+
+    return res.status(200).send({
+        message: "Emoção encontrada", emocao,
+    })
+
+    
 });
 
 export default emocoesRoutes
